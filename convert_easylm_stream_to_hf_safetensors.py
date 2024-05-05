dd95e0d7ef5d985cc60fa6eb8e0da375fb04cbee
Max <bolgov1458@yandex.ru>
2024-05-05 14:26:07 +0300

    Update convert_easylm_stream_to_hf_safetensors.py

--- convert_easylm_stream_to_hf_safetensors.py
+++ convert_easylm_stream_to_hf_safetensors.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-ckpt_path = argv[1]
+ckpt_path = 'gemma-2b-easylm'
 print("Using ckpt:", ckpt_path)
 
 from EasyLM.models.gemma.gemma_model import GemmaConfig, FlaxGemmaForCausalLMModule
@@ -8,9 +8,9 @@ from EasyLM.checkpoint import StreamingCheckpointer
 from EasyLM.jax_utils import get_float_dtype_by_name, match_partition_rules, make_shard_and_gather_fns, tree_apply, next_rng
 from transformers import FlaxGemmaForCausalLM
 
-_, param = StreamingCheckpointer.load_trainstate_checkpoint(load_from=f'trainstate_params::{ckpt_path}')
+_, param = StreamingCheckpointer.load_trainstate_checkpoint(load_from=f'params::{ckpt_path}')
 
-gemma_config = GemmaConfig.from_pretrained("google/gemma-7b")
+gemma_config = GemmaConfig.from_pretrained("google/gemma-2b")
 
 # EasyLM Gemma
 # model = FlaxGemmaForCausalLMModule(
@@ -32,19 +32,19 @@ auto_model = FlaxGemmaForCausalLM(config=gemma_config) # HF Gemma
 auto_model.params = params['params']
 
 # 단일 파일로 로드해야 함
-auto_model.save_pretrained('./flax-gemma-ko-8b', max_shard_size='999GB')
+auto_model.save_pretrained('./gemma-interm-hf', max_shard_size='999GB')
 
 # HF Flax --> HF SafeTensors
 import torch
 from transformers import GemmaForCausalLM
 
-hf_model = GemmaForCausalLM.from_pretrained('./flax-gemma-ko-8b/', 
+hf_model = GemmaForCausalLM.from_pretrained('./gemma-interm-hf/', 
                                             from_flax=True)
 hf_model.to(torch.bfloat16)
 hf_model.config.torch_dtype = torch.bfloat16
-hf_model.save_pretrained('./gemma-ko-7b-dev', max_shard_size='4GB')
+hf_model.save_pretrained('./gemma-interm-hf', max_shard_size='4GB')
 
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
-tokenizer.save_pretrained('./gemma-ko-7b-dev')
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+tokenizer.save_pretrained('./gemma-interm-hf')
